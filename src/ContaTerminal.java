d507e735ea68869e0229df0dff67163008dd7286
Aline Silva <alinealv.silv@gmail.com>
2025-02-28 23:47:09 -0300

    first commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // Criando um objeto Scanner para receber entradas do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando e lendo os dados do usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibindo a mensagem final
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
